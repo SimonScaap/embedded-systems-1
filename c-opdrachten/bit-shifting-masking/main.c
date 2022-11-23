e17ac46c67a21c9388cda0a41397d66dd2a9349b
Simon Zweers <simon.zweers@hva.nl>
2022-11-23 23:26:21 +0100

    structuur code van dlo toegevoegd

--- c-opdrachten/bit-shifting-masking/main.c
+++ c-opdrachten/bit-shifting-masking/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
-int main() {
-
-    printf("sum x ^ y:\n");
-    printf("carry x & y:\n");
+int bit_check(int position, int number) {
+    // TODO: Maak af
+    return 0;
 }
+
+int bit_set(int position, int value, int number) {
+    // TODO: Maak af
+    return 0;
+}
+
+int main() {
+    int number = 7;
+
+    printf("value: %d\n", number);
+    printf("value: %d\n", bit_check(0, number));
+    printf("value: %d\n", bit_check(3, number));
+    printf("value: %d\n", bit_set(0, 1, number));
+    printf("value: %d\n", bit_set(0, 0, number));
+    printf("value: %d\n", bit_set(4, 1, number));
+}
+
