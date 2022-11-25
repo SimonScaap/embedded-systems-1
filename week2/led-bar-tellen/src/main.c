6e4d64dba4ea474f9e4e0fa9327fbdb21a6da717
Simon Zweers <simon.zweers@hva.nl>
2022-11-25 11:25:48 +0100

    magic numvers verwijderd in led bar tellen

--- week2/led-bar-tellen/src/main.c
+++ week2/led-bar-tellen/src/main.c
@@ -9,18 +9,20 @@
 
 #define DIGITS 10
 
+// Delay voor de counter
+#define DELAY_MS 1000
+
 //#define LED_PINS_ARR[5] 
 
 void app_main() {
     printf("Setting up\n");
-    int led_pins_arr[5] = {GPIO_NUM_4, GPIO_NUM_5, GPIO_NUM_6, GPIO_NUM_7, GPIO_NUM_15};
-    for (size_t y = 0; y < 5; y++)
+    int led_pins_arr[DIGITS] = {GPIO_NUM_4, GPIO_NUM_5, GPIO_NUM_6, GPIO_NUM_7, GPIO_NUM_15, GPIO_NUM_16, GPIO_NUM_17, GPIO_NUM_18, GPIO_NUM_8, GPIO_NUM_3};
+    for (size_t y = 0; y < DIGITS; y++)
     {
         gpio_set_direction(led_pins_arr[y], GPIO_MODE_OUTPUT);
     }
     
-    int count = 1024;
-    int delay_ms = 1000; // Delay tussen het verhogen van de teller
+    int count = pow(2, DIGITS);
     while (true)
     {
         for (size_t x = 0; x < count; x++)
@@ -39,12 +41,18 @@ void app_main() {
                 }
             }
             printf("\n");
-            vTaskDelay(delay_ms / portTICK_PERIOD_MS);
+            vTaskDelay(DELAY_MS / portTICK_PERIOD_MS);
         }
     }
 }
 
 // Functie voor het checken van elke bit
-void checkDigits () {
+int checkDigits (int numberToCheck, int index) {
+    if (((1 << index) & numberToCheck) !=0 ) {
+        return 1;
+    } else {
+        return 0;
+    }    
+    
     
 }
