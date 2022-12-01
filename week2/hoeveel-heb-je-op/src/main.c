e4be7be9041d7a067ac395ce13eda9d173914c50
Simon Zweers <simon.zweers@hva.nl>
2022-12-01 13:34:13 +0100

    Ledbar teller Functionaliteit toegevoegd

--- week2/hoeveel-heb-je-op/src/main.c
+++ week2/hoeveel-heb-je-op/src/main.c
@@ -8,11 +8,39 @@
 #define LOWER_BUTTON 5
 #define HIGHER_BUTTON 4
 #define CALLBACK_DELAY 10
+#define LED_BAR_LENGTH 10
+
+int checkDigits (int numberToCheck, int index) {
+    if (((1 << index) & numberToCheck) !=0 ) {
+        return 1;
+    } else {
+        return 0;
+    }    
+}
+
+void setLedValues (int numberToConvert, int numOfDigits, int ledPinsArr[]) {
+    for (size_t i = 0; i < numOfDigits; i++)
+    {
+        if (numberToConvert > i) {
+            printf("1");
+            gpio_set_level(ledPinsArr[i], HIGH); // Zet een led op HIGH als de bit 1 is
+        } else {
+            printf("0");
+            gpio_set_level(ledPinsArr[i], LOW); // Zet een led op LOW als de bit 0 is
+        }
+    }
+}
 
 void app_main() {
     // Setup
     gpio_set_direction(LOWER_BUTTON, GPIO_MODE_INPUT);
     gpio_set_direction(HIGHER_BUTTON, GPIO_MODE_INPUT);
+    int ledBarPinArray[LED_BAR_LENGTH] = {15, 16, 17, 18, 8, 3, 46, 9, 10, 11};
+    for (size_t i = 0; i < LED_BAR_LENGTH; i++)
+    {
+        gpio_set_direction(ledBarPinArray[i], GPIO_MODE_OUTPUT);
+    }
+    
     int getLevelLower = 0;
     int lowerCallback = 0;
     int getLevelHigher = 0;
@@ -42,6 +70,9 @@ void app_main() {
             count = 0;
         }
 
+        // Zet led pins po count
+        setLedValues(count, LED_BAR_LENGTH, ledBarPinArray);
+
         // Print Button State
         printf("Hello World!  ");
         printf("Knopje: %d  %d | count: %d\n", getLevelLower, getLevelHigher, count);
@@ -51,7 +82,5 @@ void app_main() {
         lowerCallback = getLevelLower;
         higherCallback = getLevelHigher;
 
-    }
-    
-    
+    }  
 }
