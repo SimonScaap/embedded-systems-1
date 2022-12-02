66122c177b5b7697378eddc2bf0789b890e8f48d
Simon Zweers <simon.zweers@hva.nl>
2022-12-02 12:32:19 +0100

    Prototype code voor verkeerslicht (idk of het werkt)
    2 functies toegevoegd voor reusability

--- week3/verkeerslicht/src/main.c
+++ week3/verkeerslicht/src/main.c
@@ -9,11 +9,15 @@
 #define GREEN 6
 #define YELLOW 5
 #define RED 4
+#define BUTTON_INPUT 18
 
+// Delays voor stoplichtcycle
 #define DELAY1_MS 1000
+#define TRAFFIC_DELAY_MS 3000
 
 #define ARRAY_LENGTH 3
 int LED_PINS_ARR[3] = {GREEN, YELLOW, RED};
+
 
 void set_one_to_high(int pinToSet, int ledPinsArray[], int arrayLength) {
     for (size_t i = 0; i < arrayLength; i++)
@@ -32,19 +36,40 @@ void set_one_to_high(int pinToSet, int ledPinsArray[], int arrayLength) {
     printf("\n");
 }
 
+void traffic_light_cycle() {
+    // GREEN
+    vTaskDelay(TRAFFIC_DELAY_MS / portTICK_PERIOD_MS);
+    set_one_to_high(GREEN, LED_PINS_ARR, ARRAY_LENGTH);
+    printf("turned on GREEN");
+    vTaskDelay(TRAFFIC_DELAY_MS / portTICK_PERIOD_MS);
+    // YELLOW
+    set_one_to_high(YELLOW, LED_PINS_ARR, ARRAY_LENGTH);
+    printf("turned on YELLOW");
+    vTaskDelay(TRAFFIC_DELAY_MS / portTICK_PERIOD_MS);
+    // RED
+    set_one_to_high(RED, LED_PINS_ARR, ARRAY_LENGTH);
+    printf("turned on RED");
+}
+
 void app_main() {
     printf("Setting GPIO direction\n");
     gpio_set_direction(GREEN, GPIO_MODE_OUTPUT);
     gpio_set_direction(YELLOW, GPIO_MODE_OUTPUT);
     gpio_set_direction(RED, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUTTON_INPUT, GPIO_MODE_INPUT);
+    int buttonValue = 0;
+    int buttonValueCallback = 0;
 
+    bool breakLoop = false;
     while (true)
     {
-        set_one_to_high(GREEN, LED_PINS_ARR, ARRAY_LENGTH);
-        vTaskDelay(DELAY1_MS / portTICK_PERIOD_MS);
-        set_one_to_high(YELLOW, LED_PINS_ARR, ARRAY_LENGTH);
-        vTaskDelay(DELAY1_MS / portTICK_PERIOD_MS);
-        set_one_to_high(RED, LED_PINS_ARR, ARRAY_LENGTH);
-        vTaskDelay(DELAY1_MS / portTICK_PERIOD_MS);
+        buttonValue = gpio_get_level(BUTTON_INPUT);
+        if (buttonValue > buttonValueCallback)
+        {
+            printf("Button Pressed");
+            
+        }
+        buttonValueCallback = buttonValue;
     }
+    
 }
