8e9e0c86c9c738c91aad64543d8b69abddb1c78d
Simon Zweers <simon.zweers@hva.nl>
2022-12-15 13:18:21 +0100

    verkeerslicht improved werking met scanner toegevoegd

--- week4/verkeerslicht-improved/src/main.c
+++ week4/verkeerslicht-improved/src/main.c
@@ -1,1 +1,46 @@
-void app_main() {}
+#include "stdio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+#define HIGH 1
+#define LOW 0
+
+#define GREEN 6
+#define YELLOW 5
+#define RED 4
+#define UARTNUM UART_NUM_2
+
+#define ARRAY_LENGTH 3
+int LED_PINS_ARR[3] = {GREEN, YELLOW, RED};
+
+void set_one_to_high(int pinToSet, int ledPinsArray[], int arrayLength) {
+    for (size_t i = 0; i < arrayLength; i++)
+    {
+        if (pinToSet == ledPinsArray[i])
+        {
+            gpio_set_level(ledPinsArray[i], HIGH);
+            printf("Set %d to HIGH\t", ledPinsArray[i]);
+        } 
+        else
+        {
+            gpio_set_level(ledPinsArray[i], LOW);
+            printf("Set %d to LOW\t", ledPinsArray[i]);
+        }   
+    }
+    printf("\n");
+}
+
+void app_main() {
+    int scanfCommand = 0;
+    while (true)
+    {
+        scanfCommand = 0;
+        printf("Input data: \t");
+        scanf("%8d", &scanfCommand);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        printf("Data received: %d\n", scanfCommand);
+    }
+    
+}
