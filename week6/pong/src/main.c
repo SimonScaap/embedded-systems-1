78761878ae2cfef4dfbd218555d463a4cde59572
Simon Zweers <simon.zweers@hva.nl>
2023-01-04 22:33:02 +0100

    Data sctuct in task en valid click mechanisme

--- week6/pong/src/main.c
+++ week6/pong/src/main.c
@@ -8,11 +8,16 @@ int ledBarArray[BAR_SIZE] = {4, 5, 6, 7, 15, 16, 17, 18, 8, 3};
 
 #define LIVES_SIZE 5
 int livesArray[LIVES_SIZE] = {10, 11, 12, 13, 14};
+#define BUTTON_PLAY 46
+#define ZONE_LED 9
+
+int counter = 0;
 
 typedef struct
 {
     int buttonPin;
     int cursorIndex;
+    int lives;
 } ButtonPressData;
 
 
@@ -47,15 +52,39 @@ void update_ledbar(int ledIndex, int ledBarArray[], int totalLeds) {
     }
 }
 
-void vPressCheck(void *p) {
-    //ButtonPressData *data = (ButtonPressData*) pvParameters;
-    int pointer = (int)(int*)p;
-    while (true)
+void valid_click(int led, int zoneSize, int cursor) {
+    bool on = false;
+    for (size_t i = 0; i < zoneSize; i++)
     {
-        printf("Dit zijn de parameters: %d\n", pointer);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        if (((1 << i) & cursor) != 0)
+        {
+            on = true;
+        }
+    }
+    if (on)
+    {
+        gpio_set_level(led, 1);
+    }
+    else
+    {
+        gpio_set_level(led, 0);
     }
     
+    
+}
+
+void vPressCheck(void *p) {
+    ButtonPressData *data = (ButtonPressData*)p;
+    int button = data->buttonPin;
+    int cursorindex = data->cursorIndex;
+
+    //int* pointer = (int*)p;
+    while (true)
+    {
+        counter++;
+        printf("Dit zijn de parameters: %d, %d - %d\n", button, cursorindex, counter);
+        vTaskDelay(1500 / portTICK_PERIOD_MS);
+    }
 }
 
 void app_main() {
@@ -71,6 +100,7 @@ void app_main() {
         gpio_set_direction(livesArray[i], GPIO_MODE_OUTPUT);
         gpio_set_level(livesArray[i], 1);
     }
+    gpio_set_direction(ZONE_LED, GPIO_MODE_OUTPUT);
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
@@ -86,43 +116,46 @@ void app_main() {
     bool gameOver = false;
     int lives = 5;
     int cursorIndex = 0b01;
+    int delay = 120;
+    int delayDecay = 3;
+    int score = 0;
     
     printf("ledbarIndex: %d\n", cursorIndex);
     
-    /* Task maken
-    
+    // Button Task aanmaken
     TaskHandle_t xButtonThreadHandle = NULL;
-    int parameter = 50;
-    int* parameterPtr; 
-    parameterPtr = &parameter;
-    printf("Mem addr parameter: %d\n\n\n", (int)&parameter);
-    xTaskCreate(vPressCheck, "Button", 5000, (void *)parameter, tskIDLE_PRIORITY, &xButtonThreadHandle);
-
-    
-    */ 
-    
+    //int buttonCache;
+    ButtonPressData buttonData;
+    buttonData.buttonPin = BUTTON_PLAY;
+    buttonData.cursorIndex = 0;
+    //int parameter = 50;
+    void* buttonDataPtr; 
+    buttonDataPtr = &buttonData;
+    printf("Mem addr parameter: %d\n\n\n", (int)&buttonData);
+    xTaskCreate(vPressCheck, "Button", 5000, buttonDataPtr, tskIDLE_PRIORITY, &xButtonThreadHandle);
+ 
     // Led bar heen en weer
-    
     while (!gameOver)
     {
         // Heen
         for (size_t i = 0; i < (BAR_SIZE - 1); i++)
         {
             cursorIndex = cursorIndex << 1;
-            printf("Led number: %d\n", cursorIndex);
+            //printf("Led number: %d\n", cursorIndex);
             update_ledbar(cursorIndex, ledBarArray, BAR_SIZE);
-            vTaskDelay(100 / portTICK_PERIOD_MS);
+            valid_click(ZONE_LED, 3, cursorIndex);
+            vTaskDelay(delay / portTICK_PERIOD_MS);
         }
         // Terug
         for (size_t i = 0; i < (BAR_SIZE - 1); i++)
         {
             cursorIndex = cursorIndex >> 1;
-            printf("Led number: %d\n", cursorIndex);
+            //printf("Led number: %d\n", cursorIndex);
             update_ledbar(cursorIndex, ledBarArray, BAR_SIZE);
-            vTaskDelay(100 / portTICK_PERIOD_MS);
+            valid_click(ZONE_LED, 3, cursorIndex);
+            vTaskDelay(delay / portTICK_PERIOD_MS);
         }
-        
-        lives--;
+
         update_lives(lives, livesArray, LIVES_SIZE);
         if (lives <= 0)
         {
@@ -130,8 +163,13 @@ void app_main() {
         }
         else
         {
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            //vTaskDelay(1000 / portTICK_PERIOD_MS);
             printf("Loop Ended\n Lives: %d\n", lives);
-        }        
+            score++;
+            if (delay > 50)
+            {
+                delay = delay - delayDecay;
+            }
+        }    
     }
 }
