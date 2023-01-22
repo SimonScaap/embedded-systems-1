0f1cabfd9fe90cdaa98ad1c3fd0dedf3ff26a9a5
Simon Zweers <simon.zweers@hva.nl>
2023-01-22 23:19:57 +0100

    variabelen wisselen

--- c-opdrachten/bitwise-operators/main.c
+++ c-opdrachten/bitwise-operators/main.c
@@ -1,31 +1,60 @@
 #include <stdio.h>
 
-int main() {
-    printf("Opdracht 1:\n");
-    int x = 10;
-    int y = 5;
-    printf("x = %d | y= %d\n", x, y);
-    int mask = x ^ y;
-    x = x ^ mask;
-    y = y ^ mask;
-    printf("x = %d | y = %d\n", x, y);
-
-    printf("\nOpdracht 2:\n");
-    int a = 13;
-    int i = 0;
-
-    printf("a = %d\n", a);
-    while ((a & (1 << i)) != 0) {
-        printf("%d \n", ((a & (1 << i)) != 0));
-        printf("%d\n", a);
-        a = a | (1 << i);
-        i++;
+/**
+ * @brief Check bit value on a certain position
+ * @param position Position of the bit starting at 0
+ * @param number The number checked
+ * @return
+ * - 0 if the bit on the position is 0\return
+ * - 1 if the bit on the position is 1
+ */
+int bit_check(int position, int number) {
+    // Bit shift positie
+    if (((1<<position) & number) != 0) {
+        return 1;
+    } else {
+        return 0;
     }
-    printf("a = %d\n", a);
-    return 0;
 }
 
-/*
- * 1010
- * 0101
+/**
+ *
+ * @param position The position of the but you want to set
+ * @param value 0 or 1, The value you want to changa a but to
+ * @param number The number the bit should be set .
+ * @return
  */
+int bit_set(int position, int value, int number) {
+    // als bit van position hetzelfde is als bit van value dan zelfde
+    // als bit van position anders is als bit van value dan anders
+    if (bit_check(position, number) == value) {
+        return number;
+    } else {
+        return number ^ (1 << position);
+    }
+
+}
+
+void switcheroo(int x, int y) {
+    printf("x = %d | y = %d\n", x, y);
+
+    x = x ^ y;
+    y = y ^ x;
+    x = x ^ y;
+    printf("x = %d | y = %d\n", x, y);
+    printf("\n");
+}
+
+int main() {
+    printf("Opdracht 1:\n");
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            switcheroo(i, j);
+        }
+    }
+
+    printf("\nOpdracht 2:\n");
+
+
+    return 0;
+}
